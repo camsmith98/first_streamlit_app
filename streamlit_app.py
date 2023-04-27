a3833460802ad82c45ceaa656d737a8b6ad74eea
camsmith98 <131894576+camsmith98@users.noreply.github.com>
2023-04-27 10:48:04 +0100

    Update streamlit_app.py
    added emojis and some other breakfast options

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,9 @@ streamlit.title('Beans means Heinz')
 
 streamlit.header('Whats for brekkie today?')
 
-streamlit.text('- Nuttty Granola with Peanut Butter and Full fat Greek Yoghurt')
-streamlit.text('- Cup of tea with full-fat milk')
+streamlit.text('- 🥣 Nuttty Granola with Peanut Butter and Full fat Greek Yoghurt')
+streamlit.text('- Cup of tea with full-fat milk')   
+streamlit.header('OR')
+streamlit.text('- 🥑Avocado, Cooked Tomato, olive oil and chorizo bagels')
+streamlit.header('OR')
+streamlit.text('-🍞🐔🥗Omlette with veg and toast')
