cb5cf8b48db571dfc891b4b613c94e5daa67f197
camsmith98 <131894576+camsmith98@users.noreply.github.com>
2023-04-27 13:12:08 +0100

    Update streamlit_app.py
    added fruits selected so only they are shown after theyve been picked

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Pick list here
 
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruit_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruit_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(my_fruit_list)
