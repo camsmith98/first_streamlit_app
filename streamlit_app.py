f6910be8e542493a8a7f7627b464046a07aa9215
camsmith98 <131894576+camsmith98@users.noreply.github.com>
2023-04-27 12:50:34 +0100

    Update streamlit_app.py
    added filter avocado and strawbs

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Pick list here
 
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
