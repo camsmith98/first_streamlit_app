01fda3f558e42d5abdadf617ae41a57df94c8f11
camsmith98 <131894576+camsmith98@users.noreply.github.com>
2023-04-27 10:43:50 +0100

    Update streamlit_app.py
    added tea and bullet points

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ streamlit.title('Beans means Heinz')
 
 streamlit.header('Whats for brekkie today?')
 
-streamlit.text('Nuttty Granola with Peanut Butter and Full fat Greek Yoghurt')
+streamlit.text('- Nuttty Granola with Peanut Butter and Full fat Greek Yoghurt')
+streamlit.text('- Cup of tea with full-fat milk')
