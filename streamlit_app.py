132be6fb0cded0ad348f6f6a5c8313b959f7ac19
camsmith98 <131894576+camsmith98@users.noreply.github.com>
2023-05-03 14:14:14 +0100

    Update streamlit_app.py
    added a text_input that allows user to decide what fruit they want to see, this parameter is applied to end of url api asmwell

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,21 @@ import pandas as pd
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#Pick list here
+streamlit.header('Pick list here')
 
 fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-#Fruityvice advice
+streamlit.header('Fruityvice advice')
+
+fruit_choice = streamlit.text_input('What fruit would you like info about?','kiwi')
+streamlit.write('The user has entered',fruit_choice)
 
 import requests 
 
-fruityvice_response = requests.get('https://www.fruityvice.com/api/fruit/'+'kiwi')
+fruityvice_response = requests.get('https://www.fruityvice.com/api/fruit/'+'fruit_choice')
 
 fruity_normal = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruity_normal)
